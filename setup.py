c74b093fe465810fa077861103d1465ed022f4b0
Alouh Sperk <59327781+codewithalouh@users.noreply.github.com>
2023-01-02 15:17:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ try:
 except:
   os.system("pip3 install colorama")
   os.system("pip3 install hashlib")
+  os.system("pip3 install telegram")
   os.system("pip3 install pycryptodome")
   os.system("pip3 install python-telegram-bot")
   os.system("clear")
